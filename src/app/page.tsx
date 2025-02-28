65d67f66df211448eff56883b74bd5715df09654
Dhasarathachu <dhasarathachu4@gmail.com>
2025-02-28 22:52:27 +0530

    added aa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function App() {
           Football is a family of team sports that involve, to varying degrees, kicking a ball to score a goal. Unqualified, the word football generally means the form of football that is the most popular where the word is used. Sports commonly called football include association football (known as soccer in Australia, Canada, South Africa, the United States, and sometimes in Ireland and New Zealand); Australian rules football; Gaelic football; gridiron football (specifically American football, arena football, or Canadian football); International rules football; rugby league football; and rugby union football. These various forms of football share, to varying degrees, common origins and are known as "football codes".
         </p>
         <button className="bg-cyan-500 text-white font-semibold rounded-lg py-2 px-6 hover:bg-cyan-600 transition duration-300">
-          <a href="/home">Home</a>
+          <a href="/home">Home aaa</a>
         </button>
       </div>
     </>
